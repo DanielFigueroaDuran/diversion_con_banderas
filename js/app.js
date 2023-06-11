f1e9fe3372aa02280c7afcc6c45bf8cafdcdd177
15819045Daniel <DanielFigueroaDuran>
2023-06-11 16:00:41 +0200

    Cambio de fondo dark

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
+//------------------------Mode Dark-----------------------------
+const mode = document.querySelector("#modo");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+//const iconn = document.querySelector("img");
+//const inpu = document.querySelector("input");
+//const select = document.querySelector("select");
+
+
+
 // ---------------------------Creating Api Countries ----------------------------
-
 const container = document.querySelector(".container");
-
 const countriesUrl = " https://restcountries.com/v3.1/all/";
 
 const getData = async () => {
@@ -9,6 +17,8 @@ const getData = async () => {
     const response = await fetch(countriesUrl);
     const dataCountries = await response.json();
     //console.log(dataCountries);
+    
+    
     dataCountries.forEach((element) => {
       //console.log(element.flags.png);
       //console.log(element);
@@ -37,3 +47,13 @@ const getData = async () => {
 };
 
 getData();
+ 
+//------------------------Mode Dark-----------------------------
+
+mode.addEventListener("click", function(){
+  body.classList.toggle("dark");
+  header.classList.toggle("dark");
+  //inpu.classList.toggle("inp");
+  //select.classList.toggle("seleted");
+  //iconn.classList.toggle("icon");
+});
