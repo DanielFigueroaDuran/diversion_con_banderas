220fe609555bfc65bf6b9ffbbb990d13ab1989ac
15819045Daniel <DanielFigueroaDuran>
2023-06-11 13:41:29 +0200

    Terminado de pintado en la API

--- js/app.js
+++ js/app.js
@@ -8,21 +8,27 @@ const getData = async () => {
   try {
     const response = await fetch(countriesUrl);
     const dataCountries = await response.json();
-    console.log(dataCountries);
+    //console.log(dataCountries);
     dataCountries.forEach((element) => {
       //console.log(element.flags.png);
+      //console.log(element);
       container.innerHTML += ` 
       
               <div class="card">
                   <img class="bandera" src="${element.flags.png}" alt="" />
                   <article>
+                                          
                       <p>${element.name.common}</p>
                       <p>Población: ${element.population}</p>
-                      <p>Regíon: ${element.continents}</p>
+                      <p>Regíon: ${element.region}</p>
                       <p>Capital: ${element.capital}</p>
                   </article>
-                 
+                    <div class="btn">
+                        <a href="./pages/countries.html?name=${element.name.common}"><button type="button">Más</button></a>
+                    </div> 
+                    
               </div>
+                 
         `;
     });
   } catch (error) {
---
+++ js/countries.js
@@ -0,0 +1,49 @@
+const container = document.querySelector(".container");
+
+const parameters = window.location.search;
+const newParam = new URLSearchParams(parameters);
+const nameCountry = newParam.get("name");
+
+const countriesUrl = `https://restcountries.com/v3.1/name/${nameCountry}`;
+
+const getData = async () => {
+  try {
+    const response = await fetch(countriesUrl);
+    const dataCountries = await response.json();
+    console.log(dataCountries);
+    console.log(dataCountries[0].languages);
+    console.log(dataCountries[0].name.common);
+
+    
+    
+    //console.log(dataCountries[0].currencies);
+    
+    dataCountries.forEach((element,i) => {
+      const currencies = Object.keys(element.currencies);
+      const languages = Object.values(element.languages);
+
+      console.log(currencies);
+      container.innerHTML += `
+         <div class="card">
+                  <img class="bandera" src="${element.flags.png}" alt="" />
+                  <article>                                          
+                      <p>${element.name.common}</p>
+                      <p>Nombre en idioma nativo: ${languages}</p>
+                      <p>Población: ${element.population}</p>
+                      <p>Regíon: ${element.region}</p>
+                      <p>Subregión: ${element.subregion}</p>
+                      <p>Capital:  ${element.capital}</p>
+                      <p>Monedas: ${currencies}</p>
+                      <p>Idiomas: ${languages}</p>
+                      <p>Países con los que hace frontera: ${element.borders}</p>
+                  </article>
+                                       
+              </div>
+      `;
+    });
+  } catch (error) {
+    console.log(`Error`);
+  }
+};
+
+getData();
